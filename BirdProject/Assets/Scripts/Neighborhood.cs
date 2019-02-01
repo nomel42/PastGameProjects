08bc9bc61c6e0477616818185ad4aa4c8cc54627
Yeong-haw wang <wangElmhurst@gmail.com>
2019-01-31 19:01:18 -0600

    BirdProject
    Bird simulator

---
+++ BirdProject/Assets/Scripts/Neighborhood.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neighborhood : MonoBehaviour {
+	[Header("Set Dynamically")]
+	public List<BirdO> neighbors;
+	private SphereCollider coll;
+
+	// Use this for initialization
+	void Start () {
+		neighbors = new List<BirdO> ();
+		coll = GetComponent<SphereCollider> ();
+		coll.radius = Spawner.S.neighborDist / 2;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (coll.radius != Spawner.S.neighborDist / 2) {
+			coll.radius = Spawner.S.neighborDist / 2;
+		}
+	}
+
+	void OnTriggerEnter( Collider other ){
+		BirdO b = other.GetComponent<BirdO> ();
+		if(b != null){
+			if(neighbors.IndexOf(b) != -1){
+				neighbors.Add (b);
+			}
+		}
+	}
+
+	void OnTriggerExit( Collider other ){
+		BirdO b = other.GetComponent<BirdO> ();
+		if(b != null){
+			if(neighbors.IndexOf(b) == -1){
+				neighbors.Add (b);
+			}
+		}
+	}
+
+	public Vector3 avgPos{
+		get{
+			Vector3 avg = Vector3.zero;
+			if (neighbors.Count == 0)
+				return avg;
+			for(int i =0; i < neighbors.Count; i++){
+				avg += neighbors [i].pos;
+			}
+			avg /= neighbors.Count;
+			return avg;
+		}
+	}
+
+	public Vector3 avgVel{
+		get{ Vector3 avg = Vector3.zero;
+			if (neighbors.Count == 0)
+				return avg;
+			for (int i = 0; i < neighbors.Count; i++) {
+				avg += neighbors [i].rigid.velocity;
+			}
+			avg /= neighbors.Count;
+			return avg;
+		}
+	}
+
+	public Vector3 avgClosePos{
+		get{ 
+			Vector3 avg = Vector3.zero;
+			Vector3 delta;
+			int nearCount = 0;
+			for (int i = 0; i < neighbors.Count; i++) {
+				delta = neighbors [i].pos - transform.position;
+				if (delta.magnitude <= Spawner.S.collDist) {
+					avg += neighbors [i].pos;
+					nearCount++;
+				}
+			}
+			if (nearCount == 0)
+				return avg;
+
+			avg /= nearCount;
+			return avg;
+		}
+	}
+}
---
+++ BirdProject/Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	static public Spawner S;
+	static public List<BirdO> Bird;
+
+	[Header("Set in Inspector: Spawning")]
+	public GameObject birdoPrefab;
+	public Transform  birdAnchor;
+
+	public int number = 100;
+	public float spawnRadius = 100f;
+	public float spawnDelay = 0.1f;
+
+	[Header("Set in Inspector Birdo")]
+	public float velocity = 30f;
+	public float neighborDist = 30f;
+	public float collDist = 4f;
+	public float velMatching = 0.25f;
+	public float flockCentering = 0.2f;
+	public float collAvoid = 2f;
+	public float attractPull = 2f;
+	public float attractPush = 2f;
+	public float attractPushDist = 5f;
+
+	void Awake(){
+		S = this;
+		Bird = new List<BirdO> ();
+		InstantiateBirdO();
+	}
+
+	public void InstantiateBirdO(){
+		GameObject go = Instantiate (birdoPrefab);
+		BirdO b = go.GetComponent<BirdO> ();
+		b.transform.SetParent (birdAnchor);
+		Bird.Add (b);
+		if (Bird.Count < number) {
+			Invoke ("InstantiateBirdO", spawnDelay);
+		}
+
+	}
+
+}
