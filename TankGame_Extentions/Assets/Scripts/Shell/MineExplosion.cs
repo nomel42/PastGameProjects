cd0148b38e15b1bbcf5f9bb28c5542fdb05fef2a
Yeong-haw wang <wangElmhurst@gmail.com>
2019-01-31 19:15:00 -0600

    Tank Game
    Tank Game provide by unity with extentions. easy mode with hard mode.

---
+++ TankGame_Extentions/Assets/Scripts/Shell/MineExplosion.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class MineExplosion : MonoBehaviour
+{
+	public LayerMask m_TankMask;
+	public LayerMask m_ShellMask;
+	public ParticleSystem m_ExplosionParticles;       
+	public AudioSource m_ExplosionAudio;              
+	public float m_MaxDamage = 100f;                  
+	public float m_ExplosionForce = 1000f;                             
+	public float m_ExplosionRadius = 5f;              
+
+
+
+
+	// Find all the tanks in an area around the shell and damage them.
+	private void OnTriggerEnter(Collider other)
+	{
+
+			Collider[] colliders = Physics.OverlapSphere (transform.position, m_ExplosionRadius, m_TankMask);
+			Collider[] collidersTwo = Physics.OverlapSphere (transform.position, m_ExplosionRadius, m_ShellMask);
+			for(int i =0; i < colliders.Length; i++){
+
+				Rigidbody targetRigidbody = colliders [i].GetComponent<Rigidbody> ();
+
+				if (!targetRigidbody)
+					continue;
+
+				targetRigidbody.AddExplosionForce (m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+				TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+
+				if (!targetHealth)
+					continue;
+
+				float damage = CalculateDamage (targetRigidbody.position);
+				targetHealth.TakeDamage (damage);
+			}
+
+			for(int i =0; i < collidersTwo.Length; i++){
+
+				Rigidbody targetRigidbody = colliders [i].GetComponent<Rigidbody> ();
+
+				if (!targetRigidbody)
+					continue;
+
+				targetRigidbody.AddExplosionForce (m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+				TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+
+				if (!targetHealth)
+					continue;
+				
+				float damage = CalculateDamage (targetRigidbody.position);
+				targetHealth.TakeDamage (damage);
+			}
+
+			m_ExplosionParticles.transform.parent = null;
+			m_ExplosionParticles.Play();
+			m_ExplosionAudio.Play();
+			Destroy(m_ExplosionParticles.gameObject,m_ExplosionParticles.main.duration);
+			Destroy (gameObject);
+	}
+
+	// Calculate the amount of damage a target should take based on it's position.
+	private float CalculateDamage(Vector3 targetPosition)
+	{
+		Vector3 explosionToTarget = targetPosition - transform.position;
+
+		float explosionDistance = explosionToTarget.magnitude;
+
+		float relativeDistance = (m_ExplosionRadius - explosionDistance)/ m_ExplosionRadius;
+
+		float damage = relativeDistance * m_MaxDamage;
+
+		damage = Mathf.Max (0f, damage);
+
+		return damage;
+	}
+}
---
+++ TankGame_Extentions/Assets/YeongItem/bright.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bright : MonoBehaviour {
+	public Light myLight;
+	float startTime;
+
+	public bool changeColors = true;
+	public float colorSpeed = 4.0f;
+	public Color startColor;
+	public Color endColor;
+	public bool repeatColor = true;
+
+
+	void Start(){
+
+		myLight = GetComponent<Light> ();
+		startTime = Time.time;
+	}
+
+	void FixedUpdate () {
+		if (changeColors) {
+			if (repeatColor) {
+				float t = (Mathf.Sin (Time.time - startTime * colorSpeed));
+				myLight.color = Color.Lerp (startColor, endColor, t);
+
+			} else {
+				float t = Time.time - startTime * colorSpeed;
+				myLight.color = Color.Lerp (startColor, endColor, t);
+
+			}
+		}
+
+	}
+}
